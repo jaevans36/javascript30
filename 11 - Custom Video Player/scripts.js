8d3397a9e2d72760db5d485fa6fb967589a5248f
Jay <jaevans36@gmail.com>
2018-01-21 21:20:55 +0000

    Partially finished custom video player tutorial

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,29 @@
+// Get the elements
+
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('progress');
+const progressBar = player.querySelector('.progress__filled');
+
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+// Functions
+
+function togglePlay() {
+    video.paused ? video.play() : video.pause();
+}
+
+function changeButton() {
+    const icon = video.paused ? '►' : '❚❚';
+    toggle.textContent = icon;
+}
+
+// Event listeners
+
+toggle.addEventListener('click', togglePlay);
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', changeButton);
+video.addEventListener('paused', changeButton);
